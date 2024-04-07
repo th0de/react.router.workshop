b247d8015b6c6464d337b48312c4b97b124647e5
Elijah Thode <elijahthode@gmail.com>
2024-04-07 18:56:04 -0500

    first commit basic boiler plate removed and css added. Made changes to the App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+  <div id="container">
+      <h1>Hello React Router!</h1>
+      <div id="navbar">{/* navigation here */}</div>
+      <div id="main-section">{/* routes here */}</div>
+    </div>
+      <p>
+        Hello World
+      </p>
+    </>
+  )
+}
+
+export default App
