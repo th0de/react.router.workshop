23fe4cf8830cd0728727447eed9d7c151fa0a25e
Elijah Thode <elijahthode@gmail.com>
2024-04-07 19:07:21 -0500

    Got through second tab and hit a 405 error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { Routes, Route } from "react-router-dom";
 
 import './App.css'
 
@@ -10,11 +11,13 @@ function App() {
   <div id="container">
       <h1>Hello React Router!</h1>
       <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
-    </div>
-      <p>
-        Hello World
-      </p>
+      <div id="main-section">
+   <Routes>
+     <Route path="/blue" element={<h1>Blue</h1>} />
+     <Route path="/red" element={<h1>Red</h1>} />
+   </Routes>
+     </div>
+     </div>
     </>
   )
 }
